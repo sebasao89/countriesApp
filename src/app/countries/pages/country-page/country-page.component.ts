3d717574b07924940dbf97d6b211d316514e18ee
Sebasao <sebas.ao@outlook.com>
2023-06-12 21:40:41 -0500

    Argumentos por URL ngOnInit

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.scss']
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor( private activedRoute: ActivatedRoute ) {}
+
+  ngOnInit(): void {
+    this.activedRoute.params.subscribe( ({ id }) => {
+      console.log({ params: id})
+    })
+  }
 
 }
